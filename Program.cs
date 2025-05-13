64fd57de42f7132d30e810f12f7a53bd270d043a
Esforper <dolanbayomerfaruk@gmail.com>
2025-05-13 16:26:24 +0300

    test

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
+//test commit
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
